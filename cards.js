ae3c57d92e31152fad61976b4d92fda7b830a5b3
oleksandrrozhnetskiy <94894742+oleksandrrozhnetskiy@users.noreply.github.com>
2021-11-30 17:56:31 +0200

    new

--- cards.js
+++ cards.js
@@ -3,7 +3,16 @@ var cards = ['🥰','🦄','🍄','🦊','🍀','🐥','🪲','🐼'];
 cards.forEach(
       function(){
       	console.log("ccc");
-      }
-	)
+      })
 
-console.log("this is" + "cards")
+
+
+// console.log("this is" + "cards")
+
+// function calc(i){
+//       if (i < 1) return
+//       if (i === 1) return i
+//       return i * calc(i - 1)
+// }
+
+// console.log(calc(5))
